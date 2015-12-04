de755aeaa199a1fb5a6eede31a0584a7facc573c
Sbastien Talbot <sebastien@appventus.com>
2015-12-04 03:48:04 -0500

    Applied fixes from StyleCI

--- AvBitlyBundle.php
+++ AvBitlyBundle.php
@@ -5,19 +5,18 @@ namespace AppVentus\AvBitlyBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * AvBitlyBundle
+ * AvBitlyBundle.
  *
  * @author Thomas Beaujean <thomas@appventus.com>
- *
  */
 class AvBitlyBundle extends Bundle
 {
     /**
-     * (non-PHPdoc)
+     * (non-PHPdoc).
+     *
      * @see \Symfony\Component\HttpKernel\Bundle\Bundle::boot()
      */
     public function boot()
     {
-
     }
 }
